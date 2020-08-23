e3e143880dd190cdb0222ca815e2ff1c70d59b94
MohammadHossein <mohomah06@gmail.com>
2020-08-23 12:34:02 +0430

    Add response view component

---
+++ src/app/components/response-view/response-view.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnDestroy} from '@angular/core';
+import {HttpClientService} from "../../services/http-client.service";
+import {Subscription} from "rxjs";
+import {HttpResponse} from "../../models/http-response";
+
+@Component({
+    selector: 'response-view',
+    templateUrl: './response-view.component.html',
+    styleUrls: ['./response-view.component.css']
+})
+export class ResponseViewComponent implements OnDestroy {
+    httpResponse: HttpResponse;
+    private subscription: Subscription;
+
+    constructor(httpClientService: HttpClientService) {
+        this.httpResponse = httpClientService.httpResponse;
+        this.subscription = httpClientService.httpResponseSubject.subscribe((httpResponse) => {
+            this.httpResponse = httpResponse;
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+    }
+
+}
