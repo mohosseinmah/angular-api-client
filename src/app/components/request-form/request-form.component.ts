36348b676fa765276fea26ef5e474d250474c424
MohammadHossein <mohomah06@gmail.com>
2020-08-23 18:15:06 +0430

    - Add ace-builds dependency - Use ace code editor in request-form component for request body

--- src/app/components/request-form/request-form.component.ts
+++ src/app/components/request-form/request-form.component.ts
@@ -2,6 +2,10 @@ import {Component, OnInit} from '@angular/core';
 import {HttpClientService} from "../../services/http-client.service";
 import {HttpHeader} from "../../models/http-header";
 
+import * as ace from 'ace-builds';
+import {Ace} from 'ace-builds';
+import 'ace-builds/src-noconflict/mode-json';
+
 @Component({
     selector: 'request-form',
     templateUrl: './request-form.component.html',
@@ -12,17 +16,19 @@ export class RequestFormComponent implements OnInit {
     url: string;
     urlInputClass: string[];
     headers: HttpHeader[];
-    body: string;
+
+    jsonEditor: Ace.Editor;
 
     constructor(private client: HttpClientService) {
         this.method = "GET";
         this.url = "";
-        this.headers = [];
-        this.body = "";
+        this.headers = [{key: "Content-Type", value: "application/json"}];
         this.urlInputClass = ["form-control"];
     }
 
     ngOnInit(): void {
+        this.jsonEditor = ace.edit("request-body-editor");
+        this.jsonEditor.getSession().setMode("ace/mode/json");
     }
 
     handleUrlChange(): void {
@@ -46,6 +52,6 @@ export class RequestFormComponent implements OnInit {
             this.handleUrlChange();
             return;
         }
-        this.client.call({method: this.method, url: this.url, headers: this.headers, body: this.body});
+        this.client.call({method: this.method, url: this.url, headers: this.headers, body: this.jsonEditor.getValue()});
     }
 }
